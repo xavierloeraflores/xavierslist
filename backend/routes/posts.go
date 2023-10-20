9ffd8acfe45bc818f111d4154a5a9f0987f2e77d
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-10-20 16:33:44 -0700

    FEAT: backend/routes

---
+++ backend/routes/posts.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func PublicRoutes(a *fiber.App) {
+	route := a.Group("/api/v1/posts")
+
+	route.Get("/posts", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World from posts")
+	})
+}
