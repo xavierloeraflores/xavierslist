d52be95ef72ac632deeb137eed661c45f1fe196c
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-10-21 20:54:22 -0700

    FEAT: Posts routes

--- backend/routes/posts.go
+++ backend/routes/posts.go
@@ -10,4 +10,21 @@ func PostsRoutes(a *fiber.App) {
 	route.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World from posts")
 	})
+
+	route.Post("/", func(c *fiber.Ctx) error {
+		return c.SendString("Posted to posts:\n" + string(c.Body()))
+	})
+
+	route.Get("/:id", func(c *fiber.Ctx) error {
+		return c.SendString("Get post by id")
+	})
+
+	route.Put("/:id", func(c *fiber.Ctx) error {
+		return c.SendString("Update post by id:\n" + string(c.Body()))
+	})
+	
+	route.Delete("/:id", func(c *fiber.Ctx) error {
+		return c.SendString("Delete post by id")
+	})
+
 }
