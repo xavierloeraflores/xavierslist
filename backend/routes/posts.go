332960a211ff9f9f26689c5e78947a05cb4e7f26
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-10-23 22:55:25 -0700

    FEAT: Params from routes

--- backend/routes/posts.go
+++ backend/routes/posts.go
@@ -15,28 +15,34 @@ func PostsRoutes(a *fiber.App) {
 		return c.SendString("Posted to posts:\n" + string(c.Body()))
 	})
 
-	route.Get("/:id", func(c *fiber.Ctx) error {
-		return c.SendString("Get post by id")
+	route.Get("/:postId", func(c *fiber.Ctx) error {
+		postId := c.Params("postId")
+		return c.SendString("Get post by id  " + postId)
 	})
 
-	route.Put("/:id", func(c *fiber.Ctx) error {
-		return c.SendString("Update post by id:\n" + string(c.Body()))
+	route.Put("/:postId", func(c *fiber.Ctx) error {
+		postId := c.Params("postId")
+		return c.SendString("Update post by id:\n"+ postId + string(c.Body()))
 	})
 	
-	route.Delete("/:id", func(c *fiber.Ctx) error {
-		return c.SendString("Delete post by id: " + c.Params("id"))
+	route.Delete("/:postId", func(c *fiber.Ctx) error {
+		postId := c.Params("postId")
+		return c.SendString("Delete post by id: "+ postId + c.Params("id"))
 	})
 
 	route.Get("/category/:categoryId", func(c *fiber.Ctx) error {
-		return c.SendString("Get posts by category id: " + c.Params("categoryId"))
+		categoryId := c.Params("categoryId")
+		return c.SendString("Get posts by category id: "+ categoryId + c.Params("categoryId"))
 	})
 
 	route.Get("/subcategory/:subcategoryId", func(c *fiber.Ctx) error {
-		return c.SendString("Get posts by subcategory id: " + c.Params("subcategoryId"))
+		subcategoryId := c.Params("subcategoryId")
+		return c.SendString("Get posts by subcategory id: "+ subcategoryId + c.Params("subcategoryId"))
 	})
 
 	route.Get("/user/:userId", func(c *fiber.Ctx) error {
-		return c.SendString("Get posts by user id: " + c.Params("userId"))
+		userId := c.Params("userId")
+		return c.SendString("Get posts by user id: " + userId + c.Params("userId"))
 	})
 
 }
