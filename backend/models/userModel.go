71d9c453ff14268d7faea3c13cce8ef56aca4352
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-10-26 09:43:55 -0700

    FEAT: Added password hashing to the models

--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -1,14 +1,20 @@
 package models
 
 import (
+	"backend/utils"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
 	username string `gorm:"unique"`
-	password string
+	password []byte 
 	email string `gorm:"unique"`
 }
 
 
+func (user *User) HashPassword(password string) {
+	hashedPassword := utils.HashPassword(password)
+	user.password = hashedPassword
+}
