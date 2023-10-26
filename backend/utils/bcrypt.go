0878dcef8dca6ad8565200bc7ff81e2f76b6832b
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-10-26 09:41:15 -0700

    FEAT: bcrypt

---
+++ backend/utils/bcrypt.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+func HashPassword (password string) []byte {
+	bytePassword := []byte(password)
+	
+	hash, err := bcrypt.GenerateFromPassword(bytePassword, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return hash
+}
+
+func ComparePassword (password string, hash []byte) bool {
+	bytePassword := []byte(password)
+
+	err := bcrypt.CompareHashAndPassword(hash, bytePassword)
+	if err != nil {
+		return false
+	}
+	return true
+}
