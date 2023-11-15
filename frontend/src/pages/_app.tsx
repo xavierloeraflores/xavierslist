412c713d575537db60c4df7fb18fde03b88635ea
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-11-14 23:34:52 -0800

    FEAT: Added layout to home; Resolves #12

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { type AppType } from "next/app";
+import Layout from "~/components/layout";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default api.withTRPC(MyApp);
